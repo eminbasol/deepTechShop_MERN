e1c4103f93a1a0223488e199e27bff2c31460164
eminbasol <eminbasol08@gmail.com>
2022-03-19 16:00:51 +0300

    Install bcrypctjs

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+const bcrypt  = require('bcryptjs')
+
+const users =
+    [
+        {
+            name: 'Admin User',
+            email : 'admin@example.com',
+            password: bcrypt.hashSync('123456', 10),
+            isAdmin: true
+        },
+        {
+            name: 'Tim White',
+            email: 'tim@example.com',
+            password: bcrypt.hashSync('123456', 10),
+        },
+        {
+            name: 'John Doe',
+            email: 'john@example.com',
+            password: bcrypt.hashSync('123456', 10),
+        }
+    ]
+
+    module.exports = users
