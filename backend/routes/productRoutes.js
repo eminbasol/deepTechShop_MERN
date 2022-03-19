5bbe666b532f061e310709d8783c76ea28093ba4
eminbasol <eminbasol08@gmail.com>
2022-03-19 17:27:24 +0300

    Fetch products and error handler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,33 +4,32 @@ const asyncHandler = require('express-async-handler');
 const Product = require('../models/productModel')
 const mongoose = require('mongoose');
 
-// @desc Fetch all products
-// @route GET /api/products
-// @access public
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({})
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+router.get(
+    '/',
+    asyncHandler(async (req, res) => {
+        const products = await Product.find({})
 
-    res.json(products)
-}))
+        res.json(products)
+    })
+)
 
-// @desc Fetch single products
-// @route GET /api/products/:id
-// @access public
-router.get('/:id', asyncHandler(async (req, res) => {
-    if (mongoose.Types.ObjectId.isValid(req.params.id)) {
-        const product = await Product.findById(req.params.id);
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
+router.get(
+    '/:id',
+    asyncHandler(async (req, res) => {
+        const product = await Product.findById(req.params.id)
+
         if (product) {
-            res.json(product);
+            res.json(product)
         } else {
-            res.status(404).json({
-                message: "Product not found",
-            })
+            res.status(404)
+            throw new Error('Product not found')
         }
-    } else {
-        res.status(404).json({
-            message: "Invalid ID. Product not found",
-        })
-    }
-})
+    })
 )
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
-const colors = require('colors') 
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const express = require('express')
 const productRoutes = require('./routes/productRoutes')
+const { errorHandler, notFound } = require('./middleware/errorMiddleware')
+
 const connectDB = require('./config/db')
 
 
 connectDB()
 
-const app  = express()
+const app = express()
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.get('/', (req, res) => {
     res.send('API is running')
