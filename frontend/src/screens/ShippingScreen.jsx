9508de4b18153c37df2a87823dc0b46a743c436c
eminbasol <eminbasol08@gmail.com>
2022-03-23 16:16:54 +0300

    Shipping, payment screen & checkout steps

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
 import { saveShippingAddress } from '../actions/cartActions'
+import CheckoutSteps from '../components/CheckoutSteps'
+
 
 const ShippingScreen = () => {
 
@@ -25,7 +27,8 @@ const ShippingScreen = () => {
         navigate('/payment')
     }
 
-    return <FormContainer>
+    return <FormContainer >
+        <CheckoutSteps step1 step2 />
         <h1>Shipping</h1>
         <Form onSubmit={submitHandler} >
             <Form.Group controlId="address" className='mb-3'>
