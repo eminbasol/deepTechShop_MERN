7cb32e162ec5f93e5a0e6fc090478afadb98d43b
eminbasol <eminbasol08@gmail.com>
2022-03-23 17:20:28 +0300

    Order controller & route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const { errorHandler, notFound } = require('./middleware/errorMiddleware')
 const productRoutes = require('./routes/productRoutes')
 const userRoutes = require('./routes/userRoutes')
+const orderRoutes = require('./routes/orderRoutes')
 
 const connectDB = require('./config/db')
 
@@ -16,6 +17,7 @@ app.use(express.json())
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
