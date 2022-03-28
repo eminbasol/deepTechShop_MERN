7b881e32849fbf861f13de0a4b0028379e07de55
eminbasol <eminbasol08@gmail.com>
2022-03-28 13:15:13 +0300

    Frontend product reviews

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -113,9 +113,11 @@ const createProductReview = asyncHandler(async (req, res) => {
 
         product.reviews.push(review)
 
-        product.numReviews = product.review.length
+        product.numReviews = product.reviews.length
 
-        product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length
+        product.rating =
+            product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+            product.reviews.length
 
         await product.save()
         res.status(201).json({ message: 'Review added' })
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -16,6 +16,10 @@ import {
     PRODUCT_UPDATE_SUCCESS,
     PRODUCT_UPDATE_FAIL,
     PRODUCT_UPDATE_RESET,
+    PRODUCT_CREATE_REVIEW_REQUEST,
+    PRODUCT_CREATE_REVIEW_SUCCESS,
+    PRODUCT_CREATE_REVIEW_FAIL,
+    PRODUCT_CREATE_REVIEW_RESET,
 } from "../constants/productConstants"
 
 
@@ -84,6 +88,21 @@ export const productUpdateReducer = (state = { product: {} }, action) => {
             return { isLoading: false, isError: action.payload }
         case PRODUCT_UPDATE_RESET:
             return { product: {} }
+        default:
+            return state
+    }
+}
+
+export const productReviewCreateReducer = (state = {}, action) => {
+    switch (action.type) {
+        case PRODUCT_CREATE_REVIEW_REQUEST:
+            return { isLoading: true }
+        case PRODUCT_CREATE_REVIEW_SUCCESS:
+            return { isLoading: false, isSuccess: true }
+        case PRODUCT_CREATE_REVIEW_FAIL:
+            return { isLoading: false, isError: action.payload }
+        case PRODUCT_CREATE_REVIEW_RESET:
+            return {}
         default:
             return state
     }
